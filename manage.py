d6edbe400270233857f9e61cb8d50e23f765d7f7
Francisco Madrid <fjmadrid@gmail.com>
2016-12-11 14:53:23 -0300

    add set deploy config method

--- manage.py
+++ manage.py
@@ -1,8 +1,23 @@
 #!/usr/bin/env python3
+import json
 import os
 import sys
 
+
+def set_deploy_config():
+    deploy_config_path = os.environ.get('DEPLOY_CONFIG')
+    if deploy_config_path is None or not os.path.isfile(deploy_config_path):
+        return
+
+    with open(deploy_config_path) as file_:
+        data = json.load(file_)
+        for env_var, value in data.items():
+            os.environ.setdefault(env_var, value)
+
+
 if __name__ == "__main__":
+    set_deploy_config()
+
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "almuersos.settings.local")
     try:
         from django.core.management import execute_from_command_line
